c2c9f81d88763ce5b077e809cf09c6c6eb5803b4
caibb16 <2820875472@qq.com>
2026-05-23 15:18:48 +0800

    添加自定义路径

--- scripts/streamvln_eval_multi_gpu.sh
+++ scripts/streamvln_eval_multi_gpu.sh
@@ -1,7 +1,7 @@
 export MAGNUM_LOG=quiet HABITAT_SIM_LOG=quiet
 MASTER_PORT=$((RANDOM % 101 + 20000))
 
-CHECKPOINT="StreamVLN_Video_qwen_1_5_r2r_rxr_envdrop_scalevln"
+CHECKPOINT="/data1/code/seu004/models/StreamVLN_Video_qwen_1_5_r2r_rxr_envdrop_scalevln_v1_3"
 echo "CHECKPOINT: ${CHECKPOINT}"
 
-torchrun --nproc_per_node=8 --master_port=$MASTER_PORT streamvln/streamvln_eval.py --model_path $CHECKPOINT
+torchrun --nproc_per_node=2 --master_port=$MASTER_PORT streamvln/streamvln_eval.py --model_path $CHECKPOINT
