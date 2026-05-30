2dc6eee4fa65a964026660f7ef2c887d090c47ca
caibb16 <2820875472@qq.com>
2026-05-30 13:22:18 +0800

    update

--- scripts/eval_with_scene_graph.sh
+++ scripts/eval_with_scene_graph.sh
@@ -1,10 +1,9 @@
 export MAGNUM_LOG=quiet HABITAT_SIM_LOG=quiet
-conda activate stream_uni
 MASTER_PORT=$((RANDOM % 101 + 20000))
 
 CHECKPOINT="/data1/code/seu004/models/StreamVLN_Video_qwen_1_5_r2r_rxr_envdrop_scalevln_v1_3"
 OUTPUT="./results/with_scene_graph_run"
-EVAL_SPLIT="val_seen"
+EVAL_SPLIT="val_unseen"
 NUM_FRAMES=32
 NUM_HISTORY=8
 SG_CONFIG="config/sg_config.yaml"
@@ -15,7 +14,7 @@ echo "EVAL_SPLIT: ${EVAL_SPLIT}"
 echo "SG_CONFIG: ${SG_CONFIG}"
 echo "USE_SCENE_GRAPH: true"
 
-torchrun --nproc_per_node=2 --master_port=$MASTER_PORT streamvln/evaluate_scene_graph.py \
+torchrun --nproc_per_node=4 --master_port=$MASTER_PORT streamvln/evaluate_scene_graph.py \
     --model_path $CHECKPOINT \
     --output_path $OUTPUT \
     --eval_split $EVAL_SPLIT \
